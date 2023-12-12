a55dace0af94b7976640105486ccd729005cff11
Gitdass <gkdass6847@gmail.com>
2023-12-12 21:32:55 +0530

    Add project files.

---
+++ MywebBank/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MywebBank.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
